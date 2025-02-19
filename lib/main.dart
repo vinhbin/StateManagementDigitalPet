af628465ba1339415f7cc5b734b90c74dcc23cf0
vinhbin <binepzai2004@gmail.com>
2025-02-19 13:32:43 -0500

    Update main.dart
    update logic to where if hunger level is high, pet lose happiness. Also, when feeding pet when theyre hunger is high, theyll gain happiness

--- lib/main.dart
+++ lib/main.dart
@@ -34,9 +34,7 @@ class _DigitalPetAppState extends State<DigitalPetApp> {
 
   // Update happiness based on hunger level
   void _updateHappiness() {
-    if (hungerLevel < 30) {
-      happinessLevel = (happinessLevel - 20).clamp(0, 100);
-    } else {
+    if (hungerLevel < 70) {
       happinessLevel = (happinessLevel + 10).clamp(0, 100);
     }
   }
@@ -44,8 +42,7 @@ class _DigitalPetAppState extends State<DigitalPetApp> {
   // Increase hunger level slightly when playing with the pet
   void _updateHunger() {
     hungerLevel = (hungerLevel + 5).clamp(0, 100);
-    if (hungerLevel > 100) {
-      hungerLevel = 100;
+    if (hungerLevel > 70) {
       happinessLevel = (happinessLevel - 20).clamp(0, 100);
     }
   }
